472e5e23eac2b4036845dc378304932ce4a4e8ed
Peekoe <peekoecode@gmail.com>
2022-07-05 16:56:09 -0700

    Make some net6 changes, add invalid memory check logic, cleanup

---
+++ Exceptions/InvalidMemoryAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bf_interpreter.Exceptions
+{
+    public class InvalidMemoryAccess : Exception
+    {
+        public InvalidMemoryAccess(int _dp)
+        {
+            dp = _dp;
+        }
+
+        public int dp { get; set; }
+        public string ErrorMessage => $"The data pointer was set to {dp} which is out of bounds";
+    }
+}
